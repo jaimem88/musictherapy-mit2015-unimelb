cb0c054f31ad1b3c21e8d88ad59ed2d88e22c987
Jaime Martinez <jmartinez1@student.unimelb.edu.au>
2015-05-13 19:57:08 +1000

    Test with multiple servers

--- app/public/js/controllers/webRTC_API_functions.js
+++ app/public/js/controllers/webRTC_API_functions.js
@@ -9,28 +9,13 @@ function createPeerConnection(connectToUser) {
 	var pc_config =
 	{'iceServers':
 		[
-			//{'url': 'stun:stun.l.google.com:19302'},
-			//{'url': 'stun:23.21.150.121'},
-		/*	{
-				'url': 'turn:numb.viagenie.ca',
-				credential: 'muazkh',
-				username: 'webrtc@live.com'
-			},*/
+			{'url': 'stun:stun.l.google.com:19302'},
+			{'url': 'stun:23.21.150.121'},
 			{
 				'url': 'turn:numb.viagenie.ca',
 				credential: '@aspirina.9808',
 				username: 'jmartinez1@student.unimelb.edu.au'
-			}//,
-		/*	{
-				url: 'turn:192.158.29.39:3478?transport=udp',
-				credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-				username: '28224511:1379330808'
-			},
-			{
-				url: 'turn:192.158.29.39:3478?transport=tcp',
-				credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-				username: '28224511:1379330808'
-			}*/
+			}
 		]
 	};
 	try {
