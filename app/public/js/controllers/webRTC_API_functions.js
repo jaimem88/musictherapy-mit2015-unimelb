cd2cd0c68016029fe6534864c9c30229254f0371
Jaime Martinez <jmartinez1@student.unimelb.edu.au>
2015-05-13 16:31:55 +1000

    Test with only one TURN server

--- app/public/js/controllers/webRTC_API_functions.js
+++ app/public/js/controllers/webRTC_API_functions.js
@@ -9,14 +9,14 @@ function createPeerConnection(connectToUser) {
 	var pc_config =
 	{'iceServers':
 		[
-			{'url': 'stun:stun.l.google.com:19302'},
-			{'url': 'stun:23.21.150.121'},
+			//{'url': 'stun:stun.l.google.com:19302'},
+			//{'url': 'stun:23.21.150.121'},
 			{
 				'url': 'turn:numb.viagenie.ca',
 				credential: 'muazkh',
 				username: 'webrtc@live.com'
-			},
-			{
+			}//,
+		/*	{
 				url: 'turn:192.158.29.39:3478?transport=udp',
 				credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
 				username: '28224511:1379330808'
@@ -25,7 +25,7 @@ function createPeerConnection(connectToUser) {
 				url: 'turn:192.158.29.39:3478?transport=tcp',
 				credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
 				username: '28224511:1379330808'
-			}
+			}*/
 		]
 	};
 	try {
