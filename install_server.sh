132fd0cc0bf12bd24fcbedee4b1aadd1a7ebf250
Ubuntu <ubuntu@test.novalocal>
2015-06-15 10:46:05 +1000

    Merge and fix installation script.

--- install_server.sh
+++ install_server.sh
@@ -8,8 +8,23 @@ sudo apt-get install npm
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
+#install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
 #clone and change dir
 git clone https://jmartinez1@bitbucket.org/jmartinez1/mit2015_jaime_musictherapy.git && cd mit2015_jaime_musictherapy
 sudo npm install
+sudo npm install -g forever
+#sudo forever start app.js
+#Manual Configuration
+echo "type the following commands to setup DB:"
+echo "mongo"
+echo "use MusicTherapy"
+echo "db.users.insert({fname:"admin", lname: "admin", email: "admin", password: "admin", admin:true})"
+echo "exit"
 
-#FOR MONGODB follow this tutorial http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+#start server
+sudo forever start app.js
