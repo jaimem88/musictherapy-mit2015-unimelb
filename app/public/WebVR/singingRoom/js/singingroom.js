e59d6f9921c07b5d165b07cbbe1f7c08d1620945
Jaime Martinez <jaime.martinez@clinicloud.com>
2015-09-10 22:09:02 +1000

    Webcam on 3Js working

--- app/public/WebVR/singingRoom/js/singingroom.js
+++ app/public/WebVR/singingRoom/js/singingroom.js
@@ -1,6 +1,6 @@
 //VR Space for Singing Lessons
 var container, stats;
-var camera, scene, raycaster, renderer;
+var camera, scene, renderer;
 var vrEffect;
 var vrControls;
 var mouseControls;
@@ -15,133 +15,87 @@ animate();
 function init() {
 
   container = document.createElement( 'div' );
-//  container = document.id='vrMainPage';
   container.style.position = 'relative';
   document.body.appendChild( container );
 
-
   camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 10000 );
-
+  //camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
+//  camera = new THREE.CubeCamera( 1, 100000, 128 );
+//  scene.add( cubeCamera )
   scene = new THREE.Scene();
+  scene.add(camera);
+  camera.position.set(0,150,400);
+  camera.lookAt(scene.position);
+	videoImage = document.getElementById( 'videoImage' );
+  //New gemoetry for the cube
+  var geometry = new THREE.BoxGeometry(100 , 100, 20 );
+  //Material to fill the cube, in this case a color.
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff0f } );
+  // fill the geo with material so that it renders a cube
+  var cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+  cube.position.set(200,300,0);
 
-  var light = new THREE.DirectionalLight( 0xffffff, 1 );
-  light.position.set( 1, 1, 1 ).normalize();
-  scene.add( light );
-
-  var geometry = new THREE.BoxGeometry( 20, 20, 20 );
-
-  for ( var i = 0; i < 2000; i ++ ) {
-
-    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-
-    object.position.x = Math.random() * 800 - 400;
-    object.position.y = Math.random() * 800 - 400;
-    object.position.z = Math.random() * 800 - 400;
-
-    object.rotation.x = Math.random() * 2 * Math.PI;
-    object.rotation.y = Math.random() * 2 * Math.PI;
-    object.rotation.z = Math.random() * 2 * Math.PI;
-
-    object.scale.x = Math.random() + 0.5;
-    object.scale.y = Math.random() + 0.5;
-    object.scale.z = Math.random() + 0.5;
-
-    scene.add( object );
-
-  }
-
-  raycaster = new THREE.Raycaster();
+  //var light = new THREE.PointLight(0xfff0ff);
+  //light.position.set(0,250,0);
+  //scene.add(light);
 
   renderer = new THREE.WebGLRenderer( { antialias: true } );
-  //renderer.setPixelRatio( window.devicePixelRatio );
-  //renderer.domElement.style.position='relative';
 
-//  var fullScreenButton = document.querySelector( '.full-screen' );
-//  var mouseLookButton = document.querySelector( '.mouse-look' );
-  var menu = document.createElement( 'div' );
-  menu.setAttribute('class','menu');
-  var mouseLookButton = document.createElement( 'div' );
-  mouseLookButton.setAttribute('class','button full-screen');
-  mouseLookButton.innerHTML = 'Enable Mouse Look';
-  var fullScreenButton = document.createElement( 'div' );
-  fullScreenButton.setAttribute('class','button mouse-look');
-  fullScreenButton.innerHTML = 'Start VR Mode';
-
-  menu.appendChild(mouseLookButton);
-  menu.appendChild(fullScreenButton);
-  var mouseLook = false;
-
-  fullScreenButton.onclick = function() {
-    vrEffect.setFullScreen( true );
-  };
-
-  vrControls = new THREE.VRControls(camera);
-
-  headControls = vrControls;
-
-  mouseLookButton.onclick = function() {
-    mouseLook = !mouseLook;
-
-    if (mouseLook) {
-      mouseControls = new THREE.MouseControls(camera);
-      headControls = mouseControls;
-      mouseLookButton.classList.add('enabled');
-    } else {
-      headControls = vrControls;
-      mouseLookButton.classList.remove('enabled');
-    }
-  }
-
-  vrEffect = new THREE.VREffect(renderer, VREffectLoaded);
-  function VREffectLoaded(error) {
-    if (error) {
-      fullScreenButton.innerHTML = error;
-      fullScreenButton.classList.add('error');
-    }
-  }
-
+  buttons();
   renderer.setClearColor( 0xf0f0f0 );
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.sortObjects = false;
-  container.appendChild( renderer.domElement );
+  //renderer.domElement.style.position = 'inherit'
 
-  stats = new Stats();
-//  stats.domElement.style.top = '0px';
-  //stats.domElement.style.position='absolute';
-
-  var mystats = document.createElement( 'div' );
-  mystats.setAttribute('class','mystats');
-  mystats.appendChild( stats.domElement );
-
-
-  //mouseLookButton.style.top = '0px';                               // Append the text to <button>
-  container.appendChild(menu);
-  container.appendChild(mystats);
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
   window.addEventListener( 'resize', onWindowResize, false );
+
+
+  ///////////
+	// VIDEO //
+	///////////
+	video = document.getElementById( 'monitor' );
+
+	videoImage = document.getElementById( 'videoImage' );
+	videoImageContext = videoImage.getContext( '2d' );
+	// background color if no video present
+	videoImageContext.fillStyle = '#000000';
+	videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
+	videoTexture = new THREE.Texture( videoImage );
+	videoTexture.minFilter = THREE.LinearFilter;
+	videoTexture.magFilter = THREE.LinearFilter;
+
+	var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
+	// the geometry on which the movie will be displayed;
+	// 		movie image will be scaled to fit these dimensions.
+	var movieGeometry = new THREE.PlaneBufferGeometry( 100, 100, 20, 1 );
+	var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
+	movieScreen.position.set(-200,300,0);
+	scene.add(movieScreen);
+  container.appendChild( renderer.domElement );
+	camera.position.set(0,200,200);
+	camera.lookAt(movieScreen.position);
 }
 
 
 function animate() {
 
   requestAnimationFrame( animate );
-
   render();
   stats.update();
 
 }
 
 function render() {
-
-  theta += 0.1;
-
-  camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
-  camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
-  camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
-  camera.lookAt( scene.position );
-  camera.updateMatrixWorld();
-
-
+  //Update local Camera
+  if ( video.readyState === video.HAVE_ENOUGH_DATA )
+  	{
+  		videoImageContext.drawImage( video, 0, 0, videoImage.width, videoImage.height );
+  		if ( videoTexture )
+  			videoTexture.needsUpdate = true;
+  	}
+  	renderer.render( scene, camera );
   headControls.update();
   vrEffect.render( scene, camera );
 
@@ -149,7 +103,62 @@ function render() {
 
 
 //helpers
+function buttons(){
+  // Buttons
+    var menu = document.createElement( 'div' );
+    menu.setAttribute('class','menu');
+    var mouseLookButton = document.createElement( 'div' );
+    mouseLookButton.setAttribute('class','button full-screen');
+    mouseLookButton.innerHTML = 'Enable Mouse Look';
+    var fullScreenButton = document.createElement( 'div' );
+    fullScreenButton.setAttribute('class','button mouse-look');
+    fullScreenButton.innerHTML = 'Start VR Mode';
 
+    menu.appendChild(mouseLookButton);
+    menu.appendChild(fullScreenButton);
+
+
+    var mouseLook = false;
+
+    fullScreenButton.onclick = function() {
+      vrEffect.setFullScreen( true );
+    };
+
+    vrControls = new THREE.VRControls(camera);
+
+    headControls = vrControls;
+
+    mouseLookButton.onclick = function() {
+      mouseLook = !mouseLook;
+
+      if (mouseLook) {
+        mouseControls = new THREE.MouseControls(camera);
+        headControls = mouseControls;
+        mouseLookButton.classList.add('enabled');
+      } else {
+        headControls = vrControls;
+        mouseLookButton.classList.remove('enabled');
+      }
+    }
+
+    vrEffect = new THREE.VREffect(renderer, VREffectLoaded);
+    function VREffectLoaded(error) {
+      if (error) {
+        fullScreenButton.innerHTML = error;
+        fullScreenButton.classList.add('error');
+      }
+    }
+    stats = new Stats();
+    var mystats = document.createElement( 'div' );
+    mystats.setAttribute('class','mystats');
+    mystats.appendChild( stats.domElement );
+  //Threejscontainer = document.getElementById( 'ThreeJS' );
+    //Threejscontainer.appendChild( renderer.domElement )
+
+    //mouseLookButton.style.top = '0px';                               // Append the text to <button>
+    container.appendChild(menu);
+    container.appendChild(mystats);
+}
 function onWindowResize() {
 
   camera.aspect = window.innerWidth / window.innerHeight;
